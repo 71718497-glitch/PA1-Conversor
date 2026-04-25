d45096c9cad1a120fa16a3761481075e36b44986
71718497-glitch <71718497@continental.edu.pe>
2026-04-25 17:31:13 -0500

    models.py

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, Float, DateTime
+from database import Base
+import datetime
+
+class Historial(Base):
+    __tablename__ = 'historial_conversiones'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    monto_origen = Column(Float, nullable=False)
+    monto_resultado = Column(Float, nullable=False)
+    fecha = Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Conversion(id={self.id}, monto={self.monto_origen})>"
