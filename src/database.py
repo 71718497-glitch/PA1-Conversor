45f14bafb1a6f14701a713d0629195fb166fd6d2
Harol Antony Porras Lume <tu_correo@ejemplo.com>
2026-04-25 20:18:49 -0500

    Fix CI dependencies

--- src/database.py
+++ src/database.py
@@ -1,13 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Configuración de la base de datos (SQLite)
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Función para crear tablas
 def init_db():
     Base.metadata.create_all(bind=engine)
